be0fd6820f7c8bc347da6f9eea64cf3482334939
son1cleo <108190253+son1cleo@users.noreply.github.com>
2022-08-18 22:26:30 +0600

    Add files via upload

---
+++ adding two integers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+
+	cout << "Type the first number:";
+	cin >> x;
+
+	cout << "Type the second number:";
+	cin >> y;
+
+	cout << "Summation = " << x + y;
+
+	return 0;
+}
