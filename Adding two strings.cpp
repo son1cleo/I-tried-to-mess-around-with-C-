b4c32729bb1189c7d02e2ab23c2c7bdbd9a4d3b1
son1cleo <108190253+son1cleo@users.noreply.github.com>
2022-08-18 22:27:00 +0600

    Add files via upload

---
+++ Adding two strings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{	
+	string firstName = "Midhat ";
+	string middleName = "Ratib ";
+	string lastName = "Khan ";
+
+	string fullname = firstName + middleName + lastName;
+
+	cout << fullname;
+
+	return 0;
+
+}
